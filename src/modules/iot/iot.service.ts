ba01352baa82df6516cfad0edcbc885d1bb28ec1
Xuan Dien <xdienw@gmail.com>
2025-01-27 07:34:55 +0700

    chore: remove some unimplemented functions

--- src/modules/iot/iot.service.ts
+++ src/modules/iot/iot.service.ts
@@ -1,15 +1,4 @@
 import { Injectable } from '@nestjs/common';
-import type { SensorDataEventDto } from 'modules/iot/iot.events';
-import { SocketService } from 'websocket/websocket.service';
 
 @Injectable()
-export class IoTService {
-  constructor(socketService: SocketService) {
-    this.socketService = socketService;
-  }
-
-  async handleSensorDataMonitoring(payload: SensorDataEventDto) {
-    // Handle sensor data monitoring
-    this.socketService.emit('sensor_data_monitor', payload);
-  }
-}
+export class IoTService {}
