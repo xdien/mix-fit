4d97e17447a4d6bdde81916b77bd0a1d9b3b22c4
Xuan Dien <xdienw@gmail.com>
2025-02-05 22:09:23 +0700

    refactor: update transformTelemetry method to use 'never' type and improve timestamp parsing in LiquorKilnHandler

--- src/modules/iot/handlers/base-device.handler.ts
+++ src/modules/iot/handlers/base-device.handler.ts
@@ -5,7 +5,7 @@ import type { IDeviceHandler } from '../interfaces/device-handler.interface';
 
 @Injectable()
 export abstract class BaseDeviceHandler implements IDeviceHandler {
-  abstract transformTelemetry(payload: any): Promise<TelemetryPayloadDto>;
+  abstract transformTelemetry(payload: never): Promise<TelemetryPayloadDto>;
 
   protected validatePayload(_payload: TelemetryPayloadDto): boolean {
     // Implement common validation logic
