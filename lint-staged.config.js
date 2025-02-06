bd037f1d0e7454a444840e2a5a43cf14581faf9c
Xuan Dien <xdienw@gmail.com>
2025-02-06 19:37:36 +0700

    docs: add build and deploy status badges to README

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -8,6 +8,6 @@ module.exports = {
   '{!(package)*.json,*.code-snippets,.!(browserslist)*rc}': [
     'yarn lint:prettier --parser json',
   ],
-  'package.json': ['yarn lint:prettier'],
-  '*.md': ['yarn lint:markdownlint', 'yarn lint:prettier'],
+  //   'package.json': ['yarn lint:prettier'],
+  //   '*.md': ['yarn lint:markdownlint', 'yarn lint:prettier'],
 };
