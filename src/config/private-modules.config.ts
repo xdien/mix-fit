d02eebaf290e2f87e8b3f86954666e2db27dbe5d
Xuan Dien <xdienw@gmail.com>
2025-02-09 17:39:20 +0700

    chore: remove private config

--- src/config/private-modules.config.ts
+++ src/config/private-modules.config.ts
@@ -1,11 +1,9 @@
+import path from 'node:path';
+
 export const PRIVATE_MODULES = [
   {
-    path: 'src/modules/cms/cms.module',
+    path: path.resolve(__dirname, '../modules/cms/cms.module'),
     configKey: 'privateModules.cms',
-  },
-  {
-    path: 'src/modules/analytics/analytics.module',
-    configKey: 'privateModules.analytics',
   },
   // Add more private modules here
 ];
